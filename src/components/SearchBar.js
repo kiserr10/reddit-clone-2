bbbb6f9e2831331d75db6c7cdd7bb482b3f9dd15
Ross Kiser <kiserr10@gmail.com>
2017-10-25 13:46:22 -0600

    refactored components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-	constructor(){
-		super();
-		this.onInputChange = this.onInputChange.bind(this);
-		this.handleSorts = this.handleSorts.bind(this);
+	constructor(props){
+		super(props);
+
 		this.state = {
 			term: ''
 		};
 	}
 
-	onInputChange(term) {
-		this.props.searchTerm(term);
-	}
-
-	handleSorts(event){
-		this.setState({
-			filterType: event.target.value
-		});
-		this.props.sortFilter(event.target.value);
-	}
 
 	render(){
 		return (
 			<div>
+				<div className="pull-right">
+					<p><a className="btn btn-info">New Post</a></p>
+				</div>
 				<ul className="nav nav-pills">
 					<li role="presentation" className="active">
 						<input
-							onChange={event => this.onInputChange(event.target.value)}
+							value={this.state.term}
+							onChange= { event => this.setState({ term: event.target.value})}
 							type="search" className="form-control input-sm search-form" placeholder="Filter"/>
 					</li>
 					<div className="form-inline">
-						<label for="sort">Sort By</label>
-						<select className="form-control" id="sort" value={this.state.sortType} onChange={this.handleSorts}>
-							<option value="title">Title</option>
-							<option value="date">Date</option>
-							<option value="votes">Votes</option>
+						<label for="sort">  Sort by </label>
+						<select className="form-control" id="sort">
+							<option>Some text</option>
+							<option>Some text</option>
 						</select>
 					</div>
 				</ul>
--- src/index.js
+++ src/index.js
@@ -1,124 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
-import SearchBar from './components/SearchBar';
 import Header from './components/Header';
-import NewPostFormActivate from './components/NewPostFormActivate';
-import NewPostForm from './components/NewPostForm';
-import SinglePost from './components/SinglePost';
+import SearchBar from './components/SearchBar';
 import DummyPosts from './dummy-posts';
 
-class App extends Component{
-	constructor(){
-		super();
-		this.toggleNewPostForm = this.toggleNewPostForm.bind(this);
-		this.addNewPost = this.addNewPost.bind(this);
-		this.searchTerm = this.searchTerm.bind(this);
-		this.searchFilter =this.searchFilter.bind(this);
-		this.getComment = this.getComment.bind(this);
-		this.sortFilter = this.sortFilter.bind(this);
-		this.sortData = this.sortData.bind(this);
-		this.state = {
-			term: '',
-			newPostFormShow: false,
-			filterType: 'title',
-			posts: DummyPosts
-		};
-	}
 
-	toggleNewPostForm() {
-		this.state.newPostFormShow === true ?
-		this.setState({newPostFormShow: false}) :
-		this.setState({newPostFormShow: true});
-	}
+const dummyPosts = DummyPosts;
+console.log(dummyPosts);
 
-	addNewPost(post) {
-		this.setState({
-			posts: this.state.posts.concat(post)
-		});
-	}
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<SearchBar />
+		</div>
+	);
+};
 
-	sortLetters() {
-		return (a, b) => {
-			let nameA = a.title.toUpperCase();
-			let nameB = b.title.toUpperCase();
-			if (nameA < nameB) return -1;
-			if (nameA > nameB) return 1;
-			return 0;
-		};
-	}
-
-	compareTraits(prop){
-		return (a, b) =>  b[prop] - a[prop];
-	}
-
-	getComment(comment, index) {
-		this.setState({
-			posts: this.state.posts.map((post, i) => {
-				if (i === index) {
-					post.comments = post.comments.concat(comment);
-				}
-				return post;
-			})
-		});
-	}
-
-	searchTerm(term) {
-		this.setState({
-			term: term
-		});
-	}
-
-	sortFilter(sortType){
-		this.setState({
-			filterType: sortType
-		});
-	}
-
-	searchFilter(term){
-		return (x) => {
-			return x.title.toLowerCase().includes(term.toLowerCase());
-		};
-	}
-
-	sortData(filterType){
-		if (filterType === 'votes') {
-			return this.state.posts.sort(this.compareTraits('rating'));
-		} else if (filterType === 'date') {
-			return this.state.posts.sort(this.compareTraits('created'));
-		}
-		return this.state.posts.sort(this.sortLetters());
-	}
-
-	render(){
-		return(
-			<div>
-				<Header />
-				<div className="container">
-					<NewPostFormActivate
-						toggleNewPostForm={this.toggleNewPostForm}/>
-					<SearchBar
-						sortFilter={this.sortFilter}
-						searchTerm={this.searchTerm} />
-					<NewPostForm
-						toggleNewPostForm={this.toggleNewPostForm}
-						addNewPost={this.addNewPost}
-						newPostFormShow={this.state.newPostFormShow} />
-
-					{this.sortData(this.state.filterType).filter(this.searchFilter(this.state.term)).map((post, i) => {
-						return(
-						<SinglePost
-							getComment={this.getComment}
-							postId={i}
-							key={i}
-							post={post} />
-						);
-					})
-				}
-				</div>
-			</div>
-		);
-	}
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
